462107bf0688d7f401fabb97597bec55b7e764a5
bibinbabumj <bibinbabumj93@gmail.com>
2024-12-03 02:02:23 +0000

    -Add Room Db -Test cases -Insert select -update gradle and PostRepositoryImplTest.kt

--- app/src/test/java/com/bibin/babu/software/developer/peopleinneedcoding/data/repository/PostRepositoryImplTest.kt
+++ app/src/test/java/com/bibin/babu/software/developer/peopleinneedcoding/data/repository/PostRepositoryImplTest.kt
@@ -3,17 +3,22 @@ package com.bibin.babu.software.developer.peopleinneedcoding.data.repository
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import assertk.assertions.isTrue
+import com.bibin.babu.software.developer.peopleinneedcoding.data.local.UserDao
 import org.junit.jupiter.api.Assertions.*
 
 
 import com.bibin.babu.software.developer.peopleinneedcoding.data.remote.api.ApiService
 import com.bibin.babu.software.developer.peopleinneedcoding.data.remote.dto.PostResponseDto
 import com.bibin.babu.software.developer.peopleinneedcoding.domain.model.PostItem
+import com.bibin.babu.software.developer.peopleinneedcoding.domain.model.UserModel
 import com.bibin.babu.software.developer.peopleinneedcoding.domain.repository.AnalyticsLogger
 import com.bibin.babu.software.developer.peopleinneedcoding.domain.repository.LogParam
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
 import io.mockk.verify
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertTrue
 import org.junit.Before
@@ -26,12 +31,14 @@ internal class PostRepositoryImplTest {
     private lateinit var repositoryImpl: PostRepositoryImpl
     private lateinit var apiService: ApiService
     private lateinit var analyticsLogger: AnalyticsLogger
+    private lateinit var userDao: UserDao
 
     @Before
     fun setUp() {
         apiService = mockk()
         analyticsLogger = mockk(relaxed = true)
-        repositoryImpl = PostRepositoryImpl(apiService, analyticsLogger)
+        userDao = mockk()
+        repositoryImpl = PostRepositoryImpl(apiService, analyticsLogger, userDao)
 
     }
 
@@ -70,11 +77,11 @@ internal class PostRepositoryImplTest {
         val result = repositoryImpl.getPosts()
         assertThat(result.isSuccess).isTrue()
         assertThat(result.getOrNull()).isEqualTo(mockPosts)
-        verify(exactly = 0){analyticsLogger.logEvent(any(),any())}
+        verify(exactly = 0) { analyticsLogger.logEvent(any(), any()) }
     }
 
     @Test
-    fun `Empty Api response returns empty list`()=runBlocking {
+    fun `Empty Api response returns empty list`() = runBlocking {
         val mockResponse = Response.success(PostResponseDto())
         coEvery {
             apiService.getPostsApi()
@@ -117,6 +124,32 @@ internal class PostRepositoryImplTest {
                 LogParam("message", "Internal Server Error")
             )
         }
+    }
+
+    @Test
+    fun `insert user into the databse`() = runBlocking {
+        val user = UserModel(id = 1, name = "bibin babu", email = "bibin@gmail.com")
+        coEvery { userDao.insertUser(user) } returns Unit
+        repositoryImpl.insertUser(user)
+        coVerify(exactly = 1) { userDao.insertUser(user) }
+    }
+
+    @Test
+    fun `getAllUsers retrieves all users from the database`() = runBlocking {
+
+        val user1 = UserModel(id = 1, name = "Bibin ", email = "test1@gmail.com")
+        val user2 = UserModel(id = 2, name = "Bbuu ", email = "test2@gmail.com")
+        val users = listOf(user1, user2)
+
+        coEvery { userDao.getAllUsers() } returns flowOf(users)
+
+
+        val result = repositoryImpl.getAllUserDb().first()
+
+
+        assertThat(result).isEqualTo(users)
+
+        coVerify { userDao.getAllUsers() }
     }
 
 }
